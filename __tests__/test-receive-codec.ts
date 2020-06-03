710087dbd638784f9b992c1ebb1bbc058519d6aa
Robert Elek <r@r2.io>
2020-06-03 21:51:42 +0700

    fixed test for puppeteer/chromium

--- __tests__/test-receive-codec.ts
+++ __tests__/test-receive-codec.ts
@@ -5,7 +5,7 @@ beforeAll(async ()=>{
   await retry(()=>page.goto('http://127.0.0.1:9999/test-receive-codec.html'), 5, 2000);
 })
 
-// h264 may or may not be supported (in my puppeteer it is reported not supported, but in chrome it is)
+// H.264 is not supported in puppeteer chromium, but check for true/false nevertheless, might change in the future
 test("H264", async ()=> {
   await expect(page).toMatchElement('span#h264', { text: /true|false/, timeout: 5000 });
 })
@@ -22,7 +22,7 @@ test("OPUS", async ()=> {
   await expect(page).toMatchElement('span#opus', { text: 'true', timeout: 5000 });
 })
 
-// h264 may or may not be supported (it is now, but as it's not mandatory, might change)
+// ISAC may or may not be supported (it is now, but as it's not mandatory, might change)
 test("ISAC", async ()=> {
   await expect(page).toMatchElement('span#isac', { text: /true|false/, timeout: 5000 });
 })
